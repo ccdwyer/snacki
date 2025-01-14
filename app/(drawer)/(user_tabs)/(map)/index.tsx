26f557cf39510ca60323fbe2d1cfa7b8923a6339
Christopher Dwyer <christopher.dwyer@bicyclehealth.com>
2025-01-13 23:50:38 -0500

    Fix web builds

--- app/(drawer)/(user_tabs)/(map)/index.tsx
+++ app/(drawer)/(user_tabs)/(map)/index.tsx
@@ -1,56 +1,24 @@
 import { Stack } from 'expo-router';
-import MapView, { Marker } from 'react-native-maps';
+import { View } from 'react-native';
 
 import { Container } from '~/components/Container';
-
-const region = {
-    latitude: 27.27327,
-    longitude: -80.342148,
-    latitudeDelta: 0.2,
-    longitudeDelta: 0.2,
-};
-
-const markers = [
-    {
-        id: 1,
-        latitude: 27.27327,
-        longitude: -80.342148,
-        title: 'Truck 1',
-        description: 'Truck 1 description',
-    },
-    {
-        id: 2,
-        latitude: 27.27327,
-        longitude: -80.442148,
-        title: 'Truck 2',
-        description: 'Truck 2 description',
-    },
-    {
-        id: 3,
-        latitude: 27.17327,
-        longitude: -80.242148,
-        title: 'Truck 3',
-        description: 'Truck 3 description',
-    },
-];
+import { Text } from '~/components/nativewindui/Text';
 
 export default function MapScreen() {
     return (
         <>
             <Stack.Screen options={{ title: 'Events' }} />
             <Container>
-                {/* <Text>Map</Text> */}
-                <MapView region={region} style={{ flex: 1 }}>
-                    {markers.map((marker) => (
-                        <Marker
-                            key={marker.id}
-                            pinColor="#00D6C8"
-                            coordinate={{ latitude: marker.latitude, longitude: marker.longitude }}
-                            title={marker.title}
-                            description={marker.description}
-                        />
-                    ))}
-                </MapView>
+                <View className="flex-1 items-center justify-center">
+                    <Text variant="title2" className="mb-2">
+                        Map View
+                    </Text>
+                    <Text className="text-center text-gray-500">
+                        The map is currently only available on mobile devices.
+                        {'\n'}
+                        Please use the mobile app to view the map.
+                    </Text>
+                </View>
             </Container>
         </>
     );
--- metro.config.js
+++ metro.config.js
@@ -1,9 +1,22 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const OriginalResolver = require('metro-resolver');
 const { withNativeWind } = require('nativewind/metro');
+
+const webBlacklistedModules = ['react-native-maps'];
 
 /** @type {import('expo/metro-config').MetroConfig} */
 // eslint-disable-next-line no-undef
 const config = getDefaultConfig(__dirname);
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+    if (platform === 'web' && webBlacklistedModules.includes(moduleName)) {
+        return {
+            type: 'empty',
+        };
+    }
+    // Default behavior for other module resolutions
+    return context.resolveRequest(context, moduleName, platform);
+};
 
 module.exports = withNativeWind(config, { input: './global.css', inlineRem: 16 });
