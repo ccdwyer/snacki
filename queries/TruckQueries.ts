75d3f3a33ed69ac231bcabe673de39ac954d265a
Christopher Dwyer <foulplay2@gmail.com>
2025-01-20 23:18:16 -0500

    add additional truck queries to be used on consumer facing screens

--- queries/TruckQueries.ts
+++ queries/TruckQueries.ts
@@ -5,27 +5,7 @@ const getTruckById = async (truckId: string) => {
     const { data, error } = await supabaseClient
         .from('food_trucks')
         .select(
-            `
-                *,
-                cuisine_types:food_truck_cuisine_types(cuisine_types(*)),
-                menus(*),
-                events:event_food_trucks(events(*))
-            `
-        )
-        .eq('id', truckId)
-        .single();
-    if (error) throw error;
-    return data;
-};
-
-export const useGetTruckById = (truckId: string) => {
-    return useQuery({
-        queryKey: ['truck', truckId],
-        queryFn: async () => {
-            const { data: truck, error } = await supabaseClient
-                .from('food_trucks')
-                .select(
-                    `*,
+            `*,
                     cuisine_types:food_truck_cuisine_types(cuisine_types(*)),
                     menus (
                         *,
@@ -35,14 +15,22 @@ export const useGetTruckById = (truckId: string) => {
                         )
                     ),
                     events:event_food_trucks(events(*))`
-                )
-                .eq('id', truckId)
-                .order('created_at', { foreignTable: 'menus' })
-                .order('created_at', { foreignTable: 'menus.menu_sections' })
-                .order('created_at', { foreignTable: 'menus.menu_sections.menu_items' })
-                .single();
+        )
+        .eq('id', truckId)
+        .order('created_at', { referencedTable: 'menus' })
+        .order('created_at', { referencedTable: 'menus.menu_sections' })
+        .order('created_at', { referencedTable: 'menus.menu_sections.menu_items' })
+        .single();
+    if (error) throw error;
+    return data;
+};
 
-            if (error) throw error;
+export const useGetTruckById = (truckId: string) => {
+    return useQuery({
+        queryKey: ['truck', truckId],
+        queryFn: async () => {
+            const truck = await getTruckById(truckId);
+
             return truck;
         },
         enabled: !!truckId,
@@ -61,4 +49,49 @@ const getUsersFavoriteTrucks = async (userId: string) => {
         .eq('user_id', userId);
     if (error) throw error;
     return data;
+};
+
+export const useGetUsersFavoriteTrucks = (userId: string) => {
+    return useQuery({
+        queryKey: ['users-favorite-trucks', userId],
+        queryFn: async () => {
+            return getUsersFavoriteTrucks(userId);
+        },
+        enabled: !!userId,
+    });
+};
+
+const getFoodTrucksWithinDistance = async ({
+    lat,
+    lng,
+    distance,
+}: {
+    lat: number;
+    lng: number;
+    distance: number;
+}) => {
+    const { data, error } = await supabaseClient.rpc('get_food_trucks_within_distance', {
+        lat_in: lat,
+        lng_in: lng,
+        distance_miles: distance,
+    });
+    if (error) throw error;
+    return data;
+};
+
+export const useGetFoodTrucksWithinDistance = ({
+    lat,
+    lng,
+    distance,
+}: {
+    lat: number;
+    lng: number;
+    distance: number;
+}) => {
+    return useQuery({
+        queryKey: ['food-trucks-within-distance', lat, lng, distance],
+        queryFn: async () => {
+            return getFoodTrucksWithinDistance({ lat, lng, distance });
+        },
+    });
 };
